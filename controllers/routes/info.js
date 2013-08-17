796bb69f2a10f407620fe90492c8b9f98bdd4c56
Andrew Betts <andrew.betts@ft.com>
2013-08-17 18:45:02 +0100

    Docs

--- controllers/routes/info.js
+++ controllers/routes/info.js
@@ -1,3 +1,8 @@
+/*
+ * Documentation controller
+ *
+ * URL controller for home page.  Displays information on how to use the service
+ */
 
 module.exports = function(req, res) {
 	res.sendfile('views/info.html');
